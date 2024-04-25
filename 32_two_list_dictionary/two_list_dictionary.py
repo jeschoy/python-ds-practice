67f030ce1eddde8651f4e0857fc3f69ca8978496
Jessica Choi <jesymchoi@gmail.com>
2024-04-24 17:14:20 -0700

    completed 31 through 33

--- 32_two_list_dictionary/two_list_dictionary.py
+++ 32_two_list_dictionary/two_list_dictionary.py
@@ -15,3 +15,9 @@ def two_list_dictionary(keys, values):
         >>> two_list_dictionary(['a', 'b', 'c'], [1, 2, 3, 4])
         {'a': 1, 'b': 2, 'c': 3}
    """
+    result = {}
+
+    for index, value in enumerate(keys):
+        result[value] = values[index] if index < len(values) else None
+
+    return result
